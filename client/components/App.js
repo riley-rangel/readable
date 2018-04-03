7c2d3817eecc6d13ca2df4e755360bf887b455b9
Riley Rangel <rrangel91@gmail.com>
2018-04-02 17:16:05 -0700

    Create ActionsButton and CreatePost components for modal to create a new post. #3

--- client/components/App.js
+++ client/components/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
+import Modal from 'react-modal'
 import * as API from '../API'
+import ActionButton from './ActionButton'
 import Category from './Category'
+import CreatePost from './CreatePost'
 import Navbar from './Navbar'
+import MdAdd from 'react-icons/lib/md/add'
 
 export default class App extends Component {
   state = {
-    categories: []
+    categories: [],
+    addPostModalOpen: false
   }
 
+  componentWillMount() {
+    Modal.setAppElement('body')
+  }
   async componentDidMount() {
     const categories = await API.getCategories()
     this.setState({ categories })
   }
+  openAddPostModal = () => this.setState({ addPostModalOpen: true })
+  closeAddPostModal = () => this.setState({ addPostModalOpen: false })
   render() {
     return (
       <div>
@@ -19,12 +29,24 @@ export default class App extends Component {
         <div className='container no-pad'>
           <div className='row'>
             <div className='twelve-col flex'>
-              {this.state.categories.map((category, i) => (
-                <Category key={i} name={category.name} />
+              {this.state.categories.map(category => (
+                <Category key={category.path} name={category.name} />
               ))}
             </div>
           </div>
         </div>
+        <ActionButton
+          action={this.openAddPostModal}
+          position={{bottom: '25px', right: '25px'}}
+          icon={<MdAdd size={30} />}
+        />
+        <Modal
+          isOpen={this.state.addPostModalOpen}
+          onRequestClose={this.closeAddPostModal}
+        >
+          {this.state.addPostModalOpen &&
+            <CreatePost categories={this.state.categories} />}
+        </Modal>
       </div>
     )
   }
---
+++ client/components/CreatePost.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { capitalize } from '../../helpers'
+import './CreatePost.css'
+
+const CreatePost = ({ categories = [] }) => {
+  return (
+    <div>
+      <div className='row'>
+        <div className='twelve-col'>
+          <h5>Create a New Post</h5>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='twelve-col' style={{margin: 0}}>
+          <form>
+            <div className='row'>
+              <div className='twelve-col'>
+                <p className='heading'>Select a Category</p>
+                <select className='category-drop-down' name='category'>
+                  {categories.map(category => (
+                    <option key={category.path} value={category.name}>
+                      {capitalize(category.name)}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='twelve-col'>
+                <p className='heading'>Add a Title</p>
+                <input type='text' name='title' placeholder='Title' />
+              </div>
+            </div>
+            <div className='row'>
+              <div className='twelve-col'>
+                <p className='heading'>Write the Post</p>
+                <textarea className='content' type='text' name='content' placeholder='Content' />
+              </div>
+            </div>
+            <div className='row'>
+              <div className='twelve-col'>
+                <div className='flex just-ctr wrap'>
+                  <button className='btn pill btn-warning'>Clear</button>
+                  <button className='btn pill btn-primary'>Submit</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CreatePost
