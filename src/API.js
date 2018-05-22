cc923fd099cfa6aebf72234a68e7a9f426f333de
Riley Rangel <rrangel91@gmail.com>
2018-05-21 22:38:03 -0700

    Merge existing component files with cra-generated files.

--- src/API.js
+++ src/API.js
@@ -1,8 +1,9 @@
 
+const BASE_URL = 'http://localhost:3001'
 const headers = { headers: { 'Authorization': 'whatever-you-want' }}
 
 export const getCategories = async () => {
-  const res = await fetch('/categories', headers)
+  const res = await fetch(`${BASE_URL}/categories`, headers)
   const data = await res.json()
   return data.categories
 }
