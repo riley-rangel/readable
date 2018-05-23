c6d71d335687effbb641a2eedfc464f369293058
Riley Rangel <rrangel91@gmail.com>
2018-05-22 21:35:30 -0700

    Rewrite <Navbar /> using material-ui lib components.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import './Navbar.css'
+import { AppBar, Grid, Toolbar, Typography } from '@material-ui/core'
 
-const Navbar = ({ title = 'Add a title!', subtitle = '' }) => {
-  return (
-    <div className='row nav-container'>
-      <div className='twelve-col flex'>
-        <img alt='' className='icon-main float-lt y-ctr' src='assets/readable-icon.png' />
-        <h2 className='float-lt'>{title}</h2>
-        {subtitle && <h5 className='float-lt subtitle'>{subtitle}</h5>}
-      </div>
-    </div>
-  )
-}
+const Navbar = () => (
+  <div>
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Typography variant="display1" color="inherit">
+          Readable
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
+)
 
 export default Navbar
