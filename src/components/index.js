676489535d8ca8607acecb231c22e72878ddc441
Riley Rangel <rrangel91@gmail.com>
2018-06-05 16:50:59 -0700

    Define TextInput component to be used with redux-form Field.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as App } from './App';
 export { default as Category } from './Category'
 export { default as CreatePost } from './CreatePost'
+export { default as TextInput } from './TextInput'
 export { default as Navbar } from './Navbar'
