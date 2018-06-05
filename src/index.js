d8c47f73ce945db5de4d7821b59b7c81dac07f70
Riley Rangel <rrangel91@gmail.com>
2018-06-05 13:24:40 -0700

    Add redux as higher-order component to app; enhance with redux-sagas.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createSagaMiddleware from 'redux-saga'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import rootReducer from './reducers';
+import sagas from './sagas'
 import { App } from './components'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware()
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const middlewares = [ sagaMiddleware ]
+
+const enhancers = composeEnhancers(
+  applyMiddleware(...middlewares),
+)
+
+const store = createStore(rootReducer, enhancers)
+
+sagaMiddleware.run(sagas)
+
+const theme = createMuiTheme()
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
