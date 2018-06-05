dfa2fb56260e4e639987b37db64ba504f2471f0b
Riley Rangel <rrangel91@gmail.com>
2018-06-05 13:22:27 -0700

    Set up root reducer with cases for get categories saga.

---
+++ src/reducers.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux'
+import { GET_CATEGORIES_FAILURE, GET_CATEGORIES_SUCCESS } from './constants'
+import initialState from './initialState'
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case GET_CATEGORIES_FAILURE:
+      console.log(action.message)
+      return state
+    case GET_CATEGORIES_SUCCESS:
+      return {
+        ...state,
+        categories: action.categories,
+      }
+    default:
+      return state
+  }
+}
+
+export default combineReducers({
+  app: reducer,
+})
