511935faf5d31864f5b90613fa310b7be03441d9
Riley Rangel <rrangel91@gmail.com>
2018-06-05 19:19:35 -0700

    Update reducer to include cases for success and fail. #3

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
-import { GET_CATEGORIES_FAILURE, GET_CATEGORIES_SUCCESS } from './constants'
+import { GET_CATEGORIES_FAILURE, GET_CATEGORIES_SUCCESS, SAVE_POST_FAILURE, SAVE_POST_SUCCESS } from './constants'
 import initialState from './initialState'
+
+
 
 function reducer(state = initialState, action) {
   switch(action.type) {
@@ -12,6 +14,19 @@ function reducer(state = initialState, action) {
       return {
         ...state,
         categories: action.categories,
+      }
+    case SAVE_POST_FAILURE:
+      console.log(action.message)
+      return state
+    case SAVE_POST_SUCCESS:
+      return {
+        ...state,
+        posts: [
+          ...state.posts,
+          {
+            ...action.post,
+          },
+        ],
       }
     default:
       return state
