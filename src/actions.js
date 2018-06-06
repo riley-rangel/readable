ae74e46bedf59745d62c776d8100b597833b200e
Riley Rangel <rrangel91@gmail.com>
2018-06-05 19:07:13 -0700

    Define action generators for different cases. #3

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,10 @@
 import {
   GET_CATEGORIES,
   GET_CATEGORIES_FAILURE,
-  GET_CATEGORIES_SUCCESS
+  GET_CATEGORIES_SUCCESS,
+  SAVE_POST,
+  SAVE_POST_FAILURE,
+  SAVE_POST_SUCCESS,
 } from './constants'
 
 export const getCategories = () => ({
@@ -15,5 +18,20 @@ export const getCategoriesSuccess = (categories) => ({
 
 export const getCategoriesFailure = (message) => ({
   type: GET_CATEGORIES_FAILURE,
+  message,
+})
+
+export const savePost = (post) => ({
+  type: SAVE_POST,
+  post,
+})
+
+export const savePostSuccess = (post) => ({
+  type: SAVE_POST_SUCCESS,
+  post,
+})
+
+export const savePostFailure = (message) => ({
+  type: SAVE_POST_FAILURE,
   message,
 })
