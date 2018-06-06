b92e8eb923765bda02889951d97b42db7917498f
Riley Rangel <rrangel91@gmail.com>
2018-06-05 17:53:43 -0700

    Define SelectField component for use with redux-form Field.

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as App } from './App';
 export { default as Category } from './Category'
 export { default as CreatePost } from './CreatePost'
-export { default as TextInput } from './TextInput'
 export { default as Navbar } from './Navbar'
+export { default as SelectField } from './SelectField'
+export { default as TextInput } from './TextInput'
