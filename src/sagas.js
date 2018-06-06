37c4000c5183dddc8961141290556614fcc4b10a
Riley Rangel <rrangel91@gmail.com>
2018-06-06 11:07:28 -0700

    Define saga to make save post api call. #3

--- src/sagas.js
+++ src/sagas.js
@@ -1,7 +1,13 @@
 import { all, call, fork, put, takeEvery } from 'redux-saga/effects';
+import { v4 as uuid4 } from 'uuid'
 import * as API from './API'
-import { getCategoriesSuccess, getCategoriesFailure } from './actions';
-import { GET_CATEGORIES } from './constants'
+import { GET_CATEGORIES, SAVE_POST } from './constants'
+import {
+  getCategoriesSuccess,
+  getCategoriesFailure,
+  savePostSuccess,
+  savePostFailure,
+} from './actions';
 
 export function* fetchCategories() {
   try {
@@ -12,10 +18,35 @@ export function* fetchCategories() {
   }
 }
 
+export function* savePost({ post: { author, body, category, title } }) {
+  try {
+    const post = yield call(API.savePost, {
+      id: uuid4(),
+      author,
+      body,
+      category,
+      timestamp: Date.now(),
+      title,
+    })
+    yield put(savePostSuccess(post))
+  } catch ({ message }) {
+    yield put(savePostFailure(message))
+  }
+}
+
 export function* fetchCategoriesWatcher() {
   yield takeEvery(GET_CATEGORIES, fetchCategories)
 }
 
+export function* savePostWatcher() {
+  yield takeEvery(SAVE_POST, savePost)
+}
+
+const watchers = [
+  fetchCategoriesWatcher,
+  savePostWatcher,
+]
+
 export default function* rootSaga() {
-  yield all([fetchCategoriesWatcher].map(fork))
+  yield all([...watchers].map(fork))
 }
