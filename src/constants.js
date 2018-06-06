833cabbfa958659c24acb973998072fdc0246f05
Riley Rangel <rrangel91@gmail.com>
2018-06-05 19:04:45 -0700

    Define constants to initiate, success and fail cases on submit post. #3

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
 export const GET_CATEGORIES = 'getCategories'
 export const GET_CATEGORIES_SUCCESS = 'getCategoriesSuccess'
 export const GET_CATEGORIES_FAILURE = 'getCategoriesFailure'
+export const SAVE_POST = 'savePost'
+export const SAVE_POST_SUCCESS = 'savePostSuccess'
+export const SAVE_POST_FAILURE = 'savePostFailure'
